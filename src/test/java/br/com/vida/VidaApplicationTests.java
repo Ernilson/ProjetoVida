1bfa1d3dcdf3560043c30e2ff4725fcfad12a6c1
ernilson <ernilson25@gmail.com>
2023-07-07 14:33:24 -0300

    add testes unitarios

--- src/test/java/br/com/vida/VidaApplicationTests.java
+++ src/test/java/br/com/vida/VidaApplicationTests.java
@@ -1,54 +1,79 @@
 package br.com.vida;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
+import br.com.vida.Controller.CadastroController;
 import br.com.vida.Entity.Cadastro;
-import br.com.vida.Service.CadastroService;
+import br.com.vida.ServiceImplement.CadastroServiceImpl;
+import io.restassured.http.ContentType;
 
-@SpringBootTest
-class VidaApplicationTests {
-	
+@WebMvcTest
+class CadastroControllerTests {
+
 	@Autowired
-	private CadastroService service;
-	
+	private CadastroController cadControl;
+
+	@MockBean
+	private CadastroServiceImpl cadService;
+
+    @BeforeEach
+    void setup() {
+		standaloneSetup(this.cadControl);
+	}
+
+    @Test
+    void deveRetornaSucesso() {
+		List<Cadastro> cad = new ArrayList<>();
+		Cadastro cad1 = new Cadastro();
+		cad1.setNome("teste1");
+		cad1.setEnder("quadra teste");
+		cad1.setEmail("teste@test4e");
+		cad.add(cad1);
+		//when da biblioteca Mockito
+		when(cadService.listAll())
+		.thenReturn(cad);
+		given()
+		.accept(ContentType.JSON)
+		//whes da biblioteca RestAssuredMockMvc
+		.when().get("/cadastros/todos")
+		.then()
+		.statusCode(HttpStatus.OK
+				.value());
+	}
 	
 	@Test
-	void contextLoads() {
-		var cad = new Cadastro();
-		 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		cad.setNome("Funicionou");
-		cad.setEnder("12/12");
-		cad.setFone("1234567");
-		cad.setEmail("teste@teste");
-		cad.setStatus("Feito");
-		Date data = new Date(); // Obtém a data atual
-	   
-	    try {
-	        data = sdf.parse("01/01/2023"); // Exemplo de data a ser inserida
-	    } catch (ParseException e) {
-	        e.printStackTrace();
-	    }
+	public void deverRetornaSucesso() {
+		Cadastro cad1 = new Cadastro();
+	    cad1.setNome("teste1");
+	    cad1.setEnder("quadra teste");
+	    cad1.setEmail("teste@teste.com");
+	    cad1.setStatus("feito");
 	    
-	    cad.setData(data);
-	    
-	    service.saveOrUpdate(cad);
-		
-		//service.saveOrUpdate(cad);
+	  //when da biblioteca Mockito
+	    when(cadService.getId(1L))
+	    .thenReturn(Optional.of(cad1));		
+		given()
+		.accept(ContentType.JSON)
+		//whes da biblioteca RestAssuredMockMvc
+		.when()
+		.get("/cadastros/{id}",1)
+		.then()
+		.statusCode(HttpStatus.OK
+				.value());
+	}
 	
-		 List<Cadastro> listaCadastros = service.listAll();
-		 //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		    for (Cadastro cadastro : listaCadastros) {
-		       // System.out.println("Todos: " + cadastro.toString());
-		        
-		        System.out.println("Data: " + sdf.format(cadastro.getData()));
-		    }		
-	}	
-
+	//--> https://www.youtube.com/watch?v=ngbKmhXDP4A
 }
